0d92a9b8298ee139010636218fd9aade0ce1e089
Melchizedek Joshua Lozano <joshuasuelenlozano.ml@gmail.com>
2025-02-03 00:16:18 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.upang.fitness_club_management_system"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.upang.fitness_club_management_system"
