ca27a55199ad2ae72d91de65bf497f0110177a67
Kyran Gabriel Josafat <kyes.josafat.up@phinmaed.com>
2025-03-28 02:04:19 +0800

    UI design

--- app/src/main/java/com/upang/fitness_club_management_system/RateTrainer.kt
+++ app/src/main/java/com/upang/fitness_club_management_system/RateTrainer.kt
@@ -23,7 +23,7 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class RateTrainer : AppCompatActivity() {
+class  RateTrainer : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
